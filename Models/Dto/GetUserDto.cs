ef0181dc261e487e802760aa7b3f2f673b8abdc7
Tadasls <tadas.laurinaitis@gmail.com>
2023-01-05 21:16:50 +0200

    Pridejau naujus DTO Modelius

---
+++ Models/Dto/GetUserDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CompetitionEventsManager.Models.Dto
+{
+    public class GetUserDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Role { get; set; }
+
+    }
+}
+
