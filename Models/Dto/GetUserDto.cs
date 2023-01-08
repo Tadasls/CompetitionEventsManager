55379d760755008eafd2cace76b88fbc9c40bdfa
Tadasls <tadas.laurinaitis@gmail.com>
2023-01-08 17:32:39 +0200

    duombazes atnaujinimas, suvedus naujus propercius modeliams

--- Models/Dto/GetUserDto.cs
+++ Models/Dto/GetUserDto.cs
@@ -4,7 +4,11 @@ namespace CompetitionEventsManager.Models.Dto
 {
     public class GetUserDto
     {
+        
         public int Id { get; set; }
+        /// <summary>
+        /// Userio prtisijungimo vardas
+        /// </summary>
         public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
