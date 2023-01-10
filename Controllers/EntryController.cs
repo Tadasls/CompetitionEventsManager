1f890d56f0ced6089ae6292bdd971be595b410c9
Tadasls <tadas.laurinaitis@gmail.com>
2023-01-10 12:52:23 +0200

    before Refactor

---
+++ Controllers/EntryController.cs
@@ -0,0 +1,64 @@
+﻿using CompetitionEventsManager.Models;
+using CompetitionEventsManager.Repository.IRepository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace CompetitionEventsManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class EntryController : ControllerBase
+    {
+       
+      
+        private readonly IEntryRepository _entryRepo;
+       
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ILogger<EntryController> _logger;
+        public EntryController(IEntryRepository repository, ILogger<EntryController> logger, IHttpContextAccessor httpContextAccessor)
+        {
+            _entryRepo = repository;
+            _httpContextAccessor = httpContextAccessor;
+            _logger = logger;
+      
+        }
+
+        [HttpGet("/api/user/{key}/Entries")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Entry>))]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [Produces(MediaTypeNames.Application.Json)]
+        public async Task<IActionResult> Get(int key)
+        {
+            // var id = _httpContextAccessor.HttpContext.Request.Query["personId"];
+            var currentUserId = int.Parse(_httpContextAccessor.HttpContext.User.Identity.Name);
+            if (currentUserId != key)
+            {
+                _logger.LogWarning("User {currentUserId} tried to access user {key} entries", currentUserId, key);
+                return Forbid();
+            }
+            var entry = await _entryRepo.GetAsync(d => d.EntryID == key); 
+
+            return Ok(entry);
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- Repository/EntryRepository.cs
+++ Repository/EntryRepository.cs
@@ -1,6 +1,7 @@
 ﻿using CompetitionEventsManager.Data;
 using CompetitionEventsManager.Models;
 using CompetitionEventsManager.Repository.IRepository;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 
 namespace CompetitionEventsManager.Repository
@@ -8,12 +9,13 @@ namespace CompetitionEventsManager.Repository
     public class EntryRepository : Repository<Entry>, IEntryRepository
     {
         private readonly DBContext _db;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public EntryRepository(DBContext db) : base(db)
+        public EntryRepository(DBContext db, IHttpContextAccessor httpContextAccessor) : base(db)
         {
             _db = db;
+            _httpContextAccessor = httpContextAccessor;
         }
-
 
 
 
@@ -33,6 +35,63 @@ namespace CompetitionEventsManager.Repository
                 }
             }
         }
+
+
+        //public IEnumerable<Entry> Get(int userId)
+        //{
+        //    var currentUserId = int.Parse(_httpContextAccessor.HttpContext.User.Identity.Name);
+
+        //    //var id = _httpContextAccessor.HttpContext.Request.Query["personId"];
+
+
+        //    var entities =
+        //        from carUser in _db.CarUser.Where(x => x.LocalUserId == currentUserId)
+        //            //from car in _context.Cars.Where(c => c.Id == carUser.CarId).DefaultIfEmpty() //left join
+        //        join car in _db.Cars on carUser.CarId equals car.Id //inner join
+        //        where carUser.LocalUserId == userId
+        //        select car;
+
+
+
+        //    return entities;
+        //}
+
+
+        //public IEnumerable<Performance> Get2(int entryID)
+        //{
+        //    // var currentUserId = int.Parse(_httpContextAccessor.HttpContext.User.Identity.Name);
+
+        //    //var id = _httpContextAccessor.HttpContext.Request.Query["personId"];
+
+
+        //    var entities =
+        //        from entry in _db.Entries.Where(x => x.EntryID == entryID)
+        //        join performance in _db.Performances on entry.EntryID equals performance.PerformanceID 
+        //        where entry.EntryID == entryID
+        //        select performance;
+
+        //    return entities;
+        //}
+
+
+        //public IEnumerable<Horse> GetHorseInfo(int horseID)
+        //{
+        //  //  var currentUserId = int.Parse(_httpContextAccessor.HttpContext.User.Identity.Name);
+
+
+        //    var entities =
+        //        from performance in _db.Performances.Where(x => x.HorseID == horseID)
+        //        join horse in _db.Horses on performance.HorseID equals horse.HorseID 
+        //        where performance.RiderID == horseID
+        //        select horse;
+        //    return entities;
+        //}
+
+
+
+
+
+
 
 
 
