30d47c5983c35ba702b05e1eb0b527f396a9edec
Tadasls <tadas.laurinaitis@gmail.com>
2023-01-17 17:54:20 +0200

    change in servises to scoped

--- Services/ServiceCollectionExtensions.cs
+++ Services/ServiceCollectionExtensions.cs
@@ -17,14 +17,14 @@ namespace CompetitionEventsManager.Services
                 throw new ArgumentNullException(nameof(services));
             }
 
-            services.AddTransient<IUserRepository, UserRepository>();
-            services.AddTransient<IRiderRepository, RiderRepository>();
-            services.AddTransient<IHorseRepository, HorseRepository>();
-            services.AddTransient<ICompetitionRepository, CompetitionRepository>();
-            services.AddTransient<IEntryRepository, EntryRepository>();
-            services.AddTransient<IEventRepository, EventRepository>();
-            services.AddTransient<INotificationRepository, NotificationRepository>();
-            services.AddTransient<IStaffRepository, StaffRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IRiderRepository, RiderRepository>();
+            services.AddScoped<IHorseRepository, HorseRepository>();
+            services.AddScoped<ICompetitionRepository, CompetitionRepository>();
+            services.AddScoped<IEntryRepository, EntryRepository>();
+            services.AddScoped<IEventRepository, EventRepository>();
+            services.AddScoped<INotificationRepository, NotificationRepository>();
+            services.AddScoped<IStaffRepository, StaffRepository>();
 
             services.AddTransient<IHorseAdapter, HorseAdapter>();
             services.AddTransient<IRiderAdapter, RiderAdapter>();
